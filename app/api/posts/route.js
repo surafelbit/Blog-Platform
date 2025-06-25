3312ebaa0bf46544c1a689ebee125cfc5c079758
Your Real Name <surafelmuhabaw17@gmail.com>
2025-06-25 13:43:45 +0300

    Fix: updated working features

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -2,7 +2,13 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
 import { cloudinary } from "@/lib/cloudinary";
-
+export async function OPTIONS() {
+  const response = new NextResponse(null, { status: 200 });
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  return response;
+}
 // GET all posts
 export async function GET() {
   try {
