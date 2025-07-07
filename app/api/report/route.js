1b4d550e64e0a7fed45bac540fb3895dddac1071
Your Real Name <surafelmuhabaw17@gmail.com>
2025-07-07 21:58:02 +0300

    push

---
+++ app/api/report/route.js
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../lib/prisma";
+import { error } from "console";
+function withCORSHeaders(response) {
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  return response;
+}
+export async function GET(req) {
+  try {
+  } catch (error) {}
+}
+export async function POST(req) {
+  try {
+    const { reason, type, postId, reporter } = await req.json();
+
+    console.log(reason, type, postId, reporter);
+    if (!reason || !type || !reporter || !postId) {
+      return withCORSHeaders(
+        NextResponse.json({ error: "Missing Fields" }, { status: 404 })
+      );
+    }
+    const report = await prisma.report.create({
+      data: {
+        reason: reason,
+        type: type,
+        Post: { connect: { id: parseInt(postId) } },
+        reporter: reporter,
+      },
+    });
+    return withCORSHeaders(
+      NextResponse.json({ message: "POST CREATED", report: report })
+    );
+  } catch (error) {
+    console.log(error);
+    return withCORSHeaders(
+      NextResponse.json({ error: "Error Occured", details: error.message })
+    );
+  }
+}
